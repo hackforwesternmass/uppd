939f90bc5b17eb018157bf076b8bcf1417bb5687
Jake Mitchell <jacob.d.mitchell@gmail.com>
2013-06-01 12:44:15 -0700

    Fix for tag schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130601193217) do
+ActiveRecord::Schema.define(:version => 20130601193639) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -94,10 +94,10 @@ ActiveRecord::Schema.define(:version => 20130601193217) do
   end
 
   create_table "tags", :force => true do |t|
-    t.integer  "doc_page_id"
     t.string   "tag"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "section_id"
   end
 
 end
