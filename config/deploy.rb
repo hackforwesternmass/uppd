f789d3048c028d6e3d0d161d35215111771a367f
Alfred Nutile <alfrednutile@gmail.com>
2013-06-01 13:49:20 -0400

    active admin in place

---
+++ config/deploy.rb
@@ -0,0 +1,47 @@
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the 
+require 'rvm/capistrano'  # Add RVM integration
+require 'bundler/capistrano'  # Add Bundler integration
+
+ssh_options[:forward_agent] = true
+set :deploy_via, :remote_cache
+set :use_sudo, false
+load 'deploy/assets'
+
+set :application, "uppd"
+set :user, "deploy"
+set :deploy_to, "/var/www/uppd"
+
+default_run_options[:pty] = true  # Must be set for the password prompt
+                                  # from git to work
+
+set :repository, "git@github.com:user/repo.git"  # Your clone URL
+set :local_repository, "file://."
+#set :deploy_via, :copy
+
+set :scm, :git
+set :branch, "master"
+default_run_options[:pty] = true
+
+
+# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "75.98.173.74"                          # Your HTTP server, Apache/etc
+role :app, "75.98.173.74"                          # This may be the same as your `Web` server
+role :db,  "75.98.173.74", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uppd::Application.config.secret_token = '51546d12b08564fae50986713cbf71995e7cf32b032b2ce7f810fca60c3cb2fc080c47d6d3bc824ae754a0aedfacce270d35d6336554d0c6450b0275f4db1764'
