084a0b2a99a7fedd8239eafc6e8d50196b9c4bc7
Alfred Nutile <al@freshjones.com>
2013-06-02 06:55:46 -0400

    server asset issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Uppd::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.precompile += %w[active_admin.css active_admin.js styles.css]
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
