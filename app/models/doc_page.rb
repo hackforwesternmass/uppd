0c89b411bb192dbca422f41dc39595d966f63885
Alfred Nutile <al@freshjones.com>
2013-06-03 07:41:00 -0400

    Inline pdf etc

--- app/models/doc_page.rb
+++ app/models/doc_page.rb
@@ -1,14 +1,11 @@
 class DocPage < ActiveRecord::Base
   attr_accessible :filing_doc_id, :pagenumber, :pagetext, :wordcount, :tag_list, :state_list
+  belongs_to :filing_doc
   acts_as_taggable
   acts_as_taggable_on :tag, :state
 
 	searchable do 
 		text :pagetext, :tag_list, :state_list
-		# will come back to this once I can deploy the solr setup
-		# text :tag_list do	
-		# 	tag_list.map(&:name)
-		# end
 	end
 
 end
