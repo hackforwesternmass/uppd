14d1835adafc3502a22c815c879c8771886c6d8f
Alfred Nutile <alfrednutile@gmail.com>
2013-06-12 18:07:48 -0400

    Recent pull caused rake db:migreate error. I this should remove that

--- db/migrate/20130605145449_set_url_queues_timestamp_defaults.rb
+++ db/migrate/20130605145449_set_url_queues_timestamp_defaults.rb
@@ -1,11 +1,11 @@
 class SetUrlQueuesTimestampDefaults < ActiveRecord::Migration
   def up
-    execute "alter table url_queues alter column updated_at set default LOCALTIMESTAMP"
-    execute "alter table url_queues alter column created_at set default LOCALTIMESTAMP"
+    #execute "alter table url_queues alter column updated_at set default LOCALTIMESTAMP"
+    #execute "alter table url_queues alter column created_at set default LOCALTIMESTAMP"
   end
 
   def down
-    execute "alter table url_queues alter column updated_at drop default"
-    execute "alter table url_queues alter column created_at drop default"
+    #execute "alter table url_queues alter column updated_at drop default"
+    #execute "alter table url_queues alter column created_at drop default"
   end
 end
