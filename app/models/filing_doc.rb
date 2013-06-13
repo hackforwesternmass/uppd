e083efb3bc26dd7d50aff79b891be4b52d02013f
Alfred Nutile <alfrednutile@gmail.com>
2013-06-13 16:00:53 -0400

    update relations so sections uses fcc_num to belong_to filing_doc"

--- app/models/filing_doc.rb
+++ app/models/filing_doc.rb
@@ -2,4 +2,5 @@ class FilingDoc < ActiveRecord::Base
   attr_accessible :filing_doc_id, :doctype, :filetype, :filing_id, :pagecount, :sha1hash, :status, :url
   belongs_to :filing
   has_many :doc_page
+  has_many :sections
 end
--- app/models/section.rb
+++ app/models/section.rb
@@ -1,4 +1,4 @@
 class Section < ActiveRecord::Base
-  attr_accessible :end_page, :start_page, :filing_doc_id
-  belongs_to :doc_page
+  attr_accessible :end_page, :start_page, :fcc_num
+  belongs_to :filing_doc, :foreign_key => 'fcc_num', :primary_key => 'fcc_num'
 end
