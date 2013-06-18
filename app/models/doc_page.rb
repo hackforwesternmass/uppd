d4341e6478bbbe6b0f67dd02e65b43d8dc2da60e
Gyepi Sam <self-github@gyepi.com>
2013-06-18 01:33:26 -0400

    add section relationship

--- app/models/doc_page.rb
+++ app/models/doc_page.rb
@@ -4,9 +4,18 @@ class DocPage < ActiveRecord::Base
   acts_as_taggable
   acts_as_taggable_on :tag, :state
 
-	searchable do 
-		text :pagetext, :state_list
-		string :tag_list, :multiple => true, :stored => true
-	end
+  searchable do 
+    text :pagetext, :state_list
+    string :tag_list, :multiple => true, :stored => true
+  end
 
+  def section
+    Section.find_by_sql([" 
+                          SELECT s.*
+                          FROM sections s
+                          JOIN filing_docs f ON s.fcc_num = f.fcc_num
+                          AND f.id = ?
+                          AND ? BETWEEN s.start_page AND s.end_page
+                         ", pagenumber, filing_doc_id]).first
+  end
 end
